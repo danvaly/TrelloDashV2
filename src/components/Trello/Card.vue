499dc345997a3ecb5df0ba1125622d0c5fbc0b14
Valentin Ghica <vali@bocris.ro>
2021-08-19 13:29:53 +0300

    Added action menu - Need to be fixed

--- src/components/Trello/Card.vue
+++ src/components/Trello/Card.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="list-card">
     <div class="list-card-details">
-      <div class="card-labels" v-if="card.labels.length">
-        <Label v-for="label in card.labels" :key="label.id" :label="label"></Label>
+      <div class="list-card-header">
+        <div class="card-labels" v-if="card.labels.length">
+          <Label v-for="label in card.labels" :key="label.id" :label="label"></Label>
+        </div>
+
+        <CardActions :card="card"></CardActions>
       </div>
+
       <a :href="card.url" target="trello" class="list-card-title" dir="auto">
         {{ card.name }}
       </a>
-      <div class="badges"><small>in <strong>{{ card.list.name }}</strong></small></div>
-      <div class="list-card-members" v-if="card.idMembers">
-        <Member v-for="memberId in card.idMembers" :key="memberId" :memberId="memberId"></Member>
+
+      <div class="list-card-footer">
+        <div class="badges"><small>in <strong>{{ card.list.name }}</strong></small></div>
+        <div class="list-card-members" v-if="card.idMembers">
+          <Member v-for="memberId in card.idMembers" :key="memberId" :memberId="memberId"></Member>
+        </div>
       </div>
     </div>
   </div>
@@ -17,12 +25,14 @@
 <script>
 import Label from '@/components/Trello/Label'
 import Member from '@/components/Trello/Member'
+import CardActions from '@/components/Trello/CardActions'
 
 export default {
   name: 'Card',
   components: {
     Label,
-    Member
+    Member,
+    CardActions
   },
   props: {
     card: Object
---
+++ src/components/Trello/CardActions.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list-card-actions">
+    <div class="dropdown">
+      <a @click="visible=!visible" type="button" id="dropdownMenuButton"
+         data-toggle="dropdown"
+         aria-haspopup="true" aria-expanded="false">
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 24 24"
+             stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"/>
+        </svg>
+      </a>
+      <div class="dropdown-menu" v-if="visible" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardActions',
+  data: function () {
+    return {
+      visible: false
+    }
+  },
+  props: {
+    card: Object
+  },
+  mounted () {
+    // console.log(this.card)
+  }
+}
+</script>
